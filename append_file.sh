e031697333a94263e897c8740c76f701ab620a71
anne <ahbauer01@gmail.com>
2015-04-04 14:33:03 -0400

    remove temp log

--- append_file.sh
+++ append_file.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 PY_DIR=$1
-OUT_FILE=$2
+LOG_FILE=$2
+OUT_FILE=$3
 
 PY=$PY_DIR/python
 
-$PY ./query_api.py $OUT_FILE
+$PY ./query_api.py $LOG_FILE $OUT_FILE
--- query_api.py
+++ query_api.py
@@ -5,16 +5,18 @@ import json
 import wget
 
 def main():
-    if len(sys.argv) != 2:
-        print 'Usage: query_api outfilename'
+    if len(sys.argv) != 3:
+        print 'Usage: query_api logfilename outfilename'
         exit(1)
-    logfilename = 'log_api'
+    logfilename = sys.argv[1]
     call = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?sort=newest&fq=source%3A%22The+New+York+Times%22+AND+document_type%3A%22article%22&api-key=8cbd506758608f03f71e955c07315a98%3A4%3A69419709'
     wget.download(call,out=logfilename)
     json_file = open(logfilename)
     json_data = json.load(json_file)
-
-    outfile = open(sys.argv[1], 'a')
+    json_file.close()
+    os.remove(logfilename)
+    
+    outfile = open(sys.argv[2], 'a')
     if len(json_data['response']['docs']) == 0:
         outfile.write( '{0}: No documents received!\n'.format(datetime.datetime.now()) )
     else:
